abe2a228b8489ae7ed9a220d7b8267520277f4cf
rmwirabua <rmwirabua@safaricom.co.ke>
2021-12-06 14:03:09 +0300

    updated the identation and daycalculator file

---
+++ daycalculator.js
@@ -0,0 +1,6 @@
+const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+function user_Action(){
+    document.user_form.action = "index.html";
+    
